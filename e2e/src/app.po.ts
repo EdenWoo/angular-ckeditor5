18d4874cbb9221daf6bb50c241bf1634fbb04208
Eden <xiaoyumqyz@gmail.com>
2019-02-25 10:38:49 +1300

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTitleText() {
+    return element(by.css('ngx-ck5-lib-root h1')).getText();
+  }
+}
